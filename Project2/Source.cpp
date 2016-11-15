2f4b0edccc0dfda677df2658b8871a7ceb62f457
Randy Du <randy.du@agfa.com>
2016-11-15 00:13:58 -0500

    Basics of guess number game

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
-
+#include <random>
+#include <time.h>
 using namespace std;
 
 int main() {
 	
-	std::cout << "Test" << endl;
+	int numbers[5] = { 1, 2, 3, 4, 5 };
+	
+	int guessNo = -1;
+	while (guessNo != numbers[2]) {
+		cout << "Guess a number: ";
+		cin >> guessNo;
+	}
+
+	cout << "You guessed correct." << endl;
 
 	system("Pause");
 	return 0;
